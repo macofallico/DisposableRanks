1ba945fd9f509b625749c3332d9fbd26045dc45c
B.Fatih KOZ <fatih.koz@gmail.com>
2021-07-08 13:43:26 +0300

    Italian Translation
    * Italian Translation (thanks @Fabietto996)

---
+++ Resources/lang/it/common.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  // Common
+  'rank'      => 'Grado',
+  'ranks'     => 'Gradi',
+  'awards'    => 'Badge',
+  'rtitle'    => 'Titolo',
+  'minhour'   => 'Ore Minime',
+  'image'     => 'Spallina',
+  'payacars'  => 'Paga Oraria Acars',
+  'paymanual' => 'Paga Oraria Manuale',
+  'restrict'  => 'Restrizioni',
+  'allowedsf' => 'Aerei Abilitati :',
+  'allowedno' => 'Niente Restrizioni',
+  'showhide'  => 'Mostra/Nascondi Aerei Abilitati',
+];
