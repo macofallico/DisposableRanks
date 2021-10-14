eccb9125f93637ec810e0874c25c3972421dc00e
Maximiliano Fallico <macofallico@hotmail.com>
2021-10-14 11:41:06 -0300

    Create common.php

---
+++ Resources/lang/es/common.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  // Common
+  'rank'      => 'Rango',
+  'ranks'     => 'Rangos',
+  'awards'    => 'Premios',
+  'rtitle'    => 'Título',
+  'minhour'   => 'Horas Requeridas',
+  'image'     => 'Charretera',
+  'payacars'  => 'Pago/Hora Acars',
+  'paymanual' => 'Pago/Hora Manual',
+  'restrict'  => 'Restricciones',
+  'allowedsf' => 'Subflotas Permitidas :',
+  'allowedno' => 'Sin Restricciones',
+  'showhide'  => 'Mostrar/Ocultar Subflotas Permitidas',
+];
